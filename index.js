a1a9211b135f0d48da3cc141524dd88f4a7fd27d
Francisco Sosa <frjasosaor@ittepic.edu.mx>
2017-09-11 07:30:02 -0700

    First Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var converter = require('number-to-words');
+console.log("En la carrera de 1km quede en "+converter.toOrdinal(3)+" lugar");  
+
+var converter = require('number-to-words');
+console.log(converter.toWords(9000));  
+
+var converter = require('number-to-words');
+console.log(converter.toWordsOrdinal(34)); 
